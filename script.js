90fac09bf2f68dba56d2ee798c90288ef4283d57
HamzaJirah <hamzajirah@gmail.com>
2018-10-18 22:25:55 +0100

    added comments

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //hide output div
 document.querySelector('#output').style.visibility = 'hidden';
 
+//select input element
 const lbsInput = document.querySelector('#lbsInput');
 
+//add event listener to input element
 lbsInput.addEventListener('input', enterWeight);
 
+//write function to listen for input
 function enterWeight(e){
   document.querySelector('#output').style.visibility = 'visible';
   let lbs = e.target.value;
