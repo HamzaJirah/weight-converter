875d2b58fcef801d000b0d1ea2a4994cd022a49d
HamzaJirah <hamzajirah@gmail.com>
2018-10-18 22:20:06 +0100

    converted lbs to kg and ounce

--- script.js
+++ script.js
@@ -5,4 +5,6 @@ lbsInput.addEventListener('input', enterWeight);
 function enterWeight(e){
   let lbs = e.target.value;
   document.querySelector('#gramsOuput').innerHTML = lbs/0.0022046;
+  document.querySelector('#kgOuput').innerHTML = lbs/2.2046;
+  document.querySelector('#ozOuput').innerHTML = lbs*16;
 }
